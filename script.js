76180d991c822b9945c9e1e347747bea2aa7e921
sulistiawatirita290-dev <sulistiawatirita290@gmail.com>
2026-09-25 19:23:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function beli(namaProduk) {
+  alert(
+    "Kamu memilih " +
+    namaProduk +
+    ". Terima kasih sudah berbelanja di Fashiva!"
+  );
+}
+
+console.log("Fashiva Fashion Store aktif!");
